bfd9e9b7a9e94e9276c830baefd01a4553e28ef2
Dmitry_Meshkov <dm_91@bk.ru>
2021-02-01 13:20:44 +0300

    Add project files.

---
+++ CompanyProj/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyProj
+{
+    class Department
+    {
+        private List<Employee> employees = new List<Employee>();
+        public string Name { get; set; }
+        public List<Employee> Employees { get => employees; set => employees = value; }
+    }
+}
---
+++ CompanyProj/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyProj
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int Salary { get; set; }
+        public Employee(string name, string lastName, int age, int salary)
+        {
+            Name = name;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+    }
+}
